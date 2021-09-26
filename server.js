a59ec4d9d0cea25ebc74304d4035bb0093c050a4
Kalyn Reid <reidkalyn361@gmail.com>
2021-09-25 22:39:33 -0700

    Added a routes folder, and server.js.

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./routes');
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
