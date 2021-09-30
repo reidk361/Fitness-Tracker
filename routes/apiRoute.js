f036c7321d2540629bede017fe45fdc5a7e4f90f
Kalyn Reid <reidkalyn361@gmail.com>
2021-09-30 16:03:41 -0700

    Fixed issues with routes.

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,32 +1,50 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const mongoose = require("mongoose");
 const { Exercise, Workout } = require("../models/index");
 
-router.get('/workouts' , async (req, res) => {
-    const aggDuration = await Workout.aggregate([{
-        $addFields: { totalDuration:
-            { $sum:  $exercises.duration}
-        }
-    }]);
+router.get("/workouts", async (req, res) => {
+  try {
+    const aggDuration = await Workout.aggregate([
+      {
+        $addFields: { totalDuration: { $sum: "$exercises.duration" } },
+      },
+    ]);
     res.status(200).json(aggDuration);
+    // await Workout.find({}).then((result) => res.status(200).json(result))
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
 });
 
-router.get('/workouts/range' , async (req, res) => {
-    const currentDay = new Date();
-    const lastWeek = currentDay.setDate(currentDay.getDate()-7);
+router.get("/workouts/range", async (req, res) => {
+  const currentTime = new Date();
+  const currentDay = new Date(currentTime.setDate(currentTime.getDate()));
+  const lastWeek = new Date(currentTime.setDate(currentTime.getDate() - 7));
+  try {
     const aggOldDuration = await Workout.aggregate([
-        { $match: { day: { $gte: lastWeek, $lt: currentDay} } },
-        {
-        $addFields: { totalDuration:
-            { $sum:  $exercises.duration}
-        }
-    }]);
+      { $match: { day: { $gte: lastWeek, $lt: currentDay } } },
+      {
+        $addFields: { totalDuration: { $sum: "$exercises.duration" } },
+      },
+    ]);
     res.status(200).json(aggOldDuration);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
 });
 
-router.post('/workouts', async (req, res) => {
-    await Workout.create({ day: Date.now(), ...req.body})
-    .then(result => console.log(result));
+router.post("/workouts", async (req, res) => {
+  try {
+    await Workout.create({ day: Date.now(), ...req.body }).then((result) => {
+      res.status(201).json(result);
+      console.log(result);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const apiRoute = require("./apiRoute");
 const htmlRoute = require("./htmlRoute")
 
-router.use("/", htmlRoute);
 router.use("/api", apiRoute);
+router.use("/", htmlRoute);
 
 module.exports = router;
